474f11c65aff83d7f1d57b5910a452b4b5b706da
Caceresenzo <caceresenzo1502@gmail.com>
2018-12-22 19:21:14 +0100

    Added utility to hide keyboard

---
+++ src/caceresenzo/android/libs/input/InputMethodUtils.java
@@ -0,0 +1,26 @@
+package caceresenzo.android.libs.input;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class InputMethodUtils {
+	
+	/**
+	 * Hide the (virtual) keyboard if any is open.
+	 * 
+	 * @param activity
+	 *            Application activity
+	 */
+	public static void hideKeyboard(Activity activity) {
+		InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+		
+		View view = activity.getCurrentFocus();
+		if (view == null) {
+			view = new View(activity);
+		}
+		
+		inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+	}
+	
+}
